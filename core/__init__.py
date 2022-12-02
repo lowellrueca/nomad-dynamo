3963c1cf98ff2f0f6d68f8ddf4e33b1801b6176f
lowellrueca <lowellrueca@gmail.com>
2022-12-02 12:39:42 +0800

    omit module that are isolated

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,3 @@
-from cobie_type_params import (
-    set_cobie_type, 
-    set_cobie_type_asset_type, 
-    set_cobie_type_category, 
-    set_cobie_type_created_by, 
-    set_cobie_type_created_on, 
-    set_cobie_type_description, 
-    set_cobie_type_name
-)
 from collections import (
     collect_families_by_category,
     collect_element_types_by_category,
@@ -15,8 +6,11 @@ from collections import (
     element_types_of_elements,
     elements_of_element_types
 )
-from type_params import set_description, generate_type_marks, set_type_marks
-from utils import convert_ft_to_mm, created_on, get_size
+from utils import (
+    convert_ft_to_mm, 
+    created_on, 
+    get_size
+)
 
 __all__  = [
     'collect_families_by_category',
@@ -25,16 +19,6 @@ __all__  = [
     'element_types_of_family',
     'element_types_of_elements',
     'elements_of_element_types',
-    'set_description',
-    'generate_type_marks',
-    'set_type_marks',
-    'set_cobie_type',
-    'set_cobie_type_asset_type',
-    'set_cobie_type_category',
-    'set_cobie_type_created_by',
-    'set_cobie_type_created_on',
-    'set_cobie_type_description',
-    'set_cobie_type_name',
     'convert_ft_to_mm',
     'created_on',
     'get_size',
