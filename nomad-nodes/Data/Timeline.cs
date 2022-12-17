81ce46f67d2fcba8288f3beb627949023c9f912a
lowellrueca <lowellrueca@gmail.com>
2022-12-17 15:41:43 +0800

    creating Timeline module

---
+++ nomad-nodes/Data/Timeline.cs
@@ -0,0 +1,12 @@
+using Data.Internal;
+
+namespace Data
+{
+    public static class Timeline
+    {
+        public static string GetCurrentDateTime()
+        {
+            return DataManager.GetCurrentDateTime();
+        }
+    }
+}
